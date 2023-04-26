42b9dad0c8b6b4bf350e19d5ca22d085e8ceabc2
Warissara1061 <122893696+Warissara1061@users.noreply.github.com>
2023-04-26 14:29:47 +0700

    Add files via upload

---
+++ gg061.cs
@@ -0,0 +1,26 @@
+class Program{
+    static void Main(string[]args){
+        int[] People = new int[2];
+    int People = int.Parse(Console.ReadLine());
+    for (int People == 0; People < 0; ++People){
+        Num [People] = int.Parse(Console.ReadLine());
+    }
+    while (true) {
+        Console.Write("Input Number: ");
+        int Number = int.Parse(Console.ReadLine());
+        if (Number == 0 || Number < 0 || Number > 0) {
+             Console.WriteLine("You've got only 1 place");
+          } else {
+            if (Number == 0 || Number < 0){
+               break;
+          } else {
+            if (Number != 0 && Number > 0){
+                Console.Write("You've got 2 place");
+            }
+          }
+       
+    } 
+            }
+            Console.Write("Finish");
+    }
+}
